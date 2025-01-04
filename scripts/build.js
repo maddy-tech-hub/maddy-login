62a187b63edce51c736f253353ab8e66a06518de
Madhava Reddy <90255418+Madhava-Reddy@users.noreply.github.com>
2025-01-04 20:25:33 +0530

    update env files

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,9 @@
 const { execSync } = require('child_process');
-const environment = process.env.ENVIRONMENT || 'development'; // Default to 'development'
+const environment = process.env.ENVIRONMENT || 'development';
 
-// Determine the correct command based on the OS
-const isWindows = process.platform === 'win32';
-
-if (environment === 'prod') {
-    const command = isWindows
-        ? 'set NODE_ENV=production&& npx webpack --config webpack.config.js --mode production'
-        : 'export NODE_ENV=production && npx webpack --config webpack.config.js --mode production';
-    execSync(command, { stdio: 'inherit' });
+if (environment === 'production') {
+    execSync('export NODE_ENV=production && npx webpack --config webpack.config.js --mode production');
 } else {
-    const command = isWindows
-        ? 'set NODE_ENV=development&& npx webpack --config webpack.config.js --mode development'
-        : 'export NODE_ENV=development && npx webpack --config webpack.config.js --mode development';
-    execSync(command, { stdio: 'inherit' });
+    execSync('export NODE_ENV=development && npx webpack --config webpack.config.js --mode development');
 }
+
